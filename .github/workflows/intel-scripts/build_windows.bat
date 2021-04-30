6cc235fa4819eec5ed1e7c219738b65cffc1e5d1
Isaac Nickaein <nickaein.i@gmail.com>
2021-05-01 02:58:13 +0430

    basekit and hpckit

--- .github/workflows/intel-scripts/build_windows.bat
+++ .github/workflows/intel-scripts/build_windows.bat
@@ -2,7 +2,7 @@ REM SPDX-FileCopyrightText: 2020 Intel Corporation
 REM
 REM SPDX-License-Identifier: MIT
 
-set LANGUAGE=%1
+set BUILD_TYPE=%1
 set VS_VER=%2
 
 IF "%VS_VER%"=="2017_build_tools" (
@@ -15,9 +15,9 @@ IF "%VS_VER%"=="2019_build_tools" (
 for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
 @call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
 
-if "%LANGUAGE%" == "build_win_icx" goto build_win_icx
-if "%LANGUAGE%" == "build_win_icl" goto build_win_icl
-if "%LANGUAGE%" == "dpc++" goto dpcpp
+if "%BUILD_TYPE%" == "build_win_icx" goto build_win_icx
+if "%BUILD_TYPE%" == "build_win_icl" goto build_win_icl
+@REM if "%LANGUAGE%" == "dpc++" goto dpcpp
 goto exit
 
 :build_win_icx
@@ -25,12 +25,14 @@ goto exit
 cmake -S . -B build_icx -G "NMake Makefiles" -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icx -DJSON_BuildTests=On -DCMAKE_BUILD_TYPE=Release
 cmake --build build_icx --parallel 10
 @REM cd build ; ctest -j 10 --output-on-failure
+set RESULT=%ERRORLEVEL%
 goto exit
 
 :build_win_icl
 cmake -S . -B build_icl -G "NMake Makefiles" -DCMAKE_C_COMPILER=icl -DCMAKE_CXX_COMPILER=icl -DJSON_BuildTests=On -DCMAKE_BUILD_TYPE=Release
 cmake --build build_icl --parallel 10
 @REM cd build ; ctest -j 10 --output-on-failure
+set RESULT=%ERRORLEVEL%
 goto exit
 
 @REM icl -O2 src\intrin_dot_sample.cpp
@@ -44,26 +46,26 @@ goto exit
 @REM icx -O2 -msse3 src\intrin_ftz_sample.cpp
 @REM intrin_dot_sample.exe && intrin_double_sample.exe && intrin_ftz_sample.exe
 @REM set /a RESULT=%RESULT%+%ERRORLEVEL%
-goto exit
+@REM goto exit
 
-:fortran
-cd oneAPI-samples\DirectProgramming\Fortran\CombinationalLogic\openmp-primes
-ifort -O2 -fpp -qopenmp src\openmp_sample.f90
-openmp_sample.exe
-set RESULT=%ERRORLEVEL%
-ifx -O2 -fpp -qopenmp src\openmp_sample.f90
-openmp_sample.exe
-set /a RESULT=%RESULT%+%ERRORLEVEL%
-goto exit
+@REM :fortran
+@REM cd oneAPI-samples\DirectProgramming\Fortran\CombinationalLogic\openmp-primes
+@REM ifort -O2 -fpp -qopenmp src\openmp_sample.f90
+@REM openmp_sample.exe
+@REM set RESULT=%ERRORLEVEL%
+@REM ifx -O2 -fpp -qopenmp src\openmp_sample.f90
+@REM openmp_sample.exe
+@REM set /a RESULT=%RESULT%+%ERRORLEVEL%
+@REM goto exit
 
-:dpcpp
-for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\tbb\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
-@call "C:\Program Files (x86)\Intel\oneAPI\tbb\%LATEST_VERSION%\env\vars.bat"
-cd oneAPI-samples\DirectProgramming\DPC++\DenseLinearAlgebra\vector-add
-nmake -f Makefile.win
-nmake -f Makefile.win run
-set RESULT=%ERRORLEVEL%
-goto exit
+@REM :dpcpp
+@REM for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\tbb\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
+@REM @call "C:\Program Files (x86)\Intel\oneAPI\tbb\%LATEST_VERSION%\env\vars.bat"
+@REM cd oneAPI-samples\DirectProgramming\DPC++\DenseLinearAlgebra\vector-add
+@REM nmake -f Makefile.win
+@REM nmake -f Makefile.win run
+@REM set RESULT=%ERRORLEVEL%
+@REM goto exit
 
 :exit
 exit /b %RESULT%
