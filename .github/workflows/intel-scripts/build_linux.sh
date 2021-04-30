98aed320371915555fd44ba0af9dfdd3d58d470a
Isaac Nickaein <nickaein.i@gmail.com>
2021-04-30 17:47:05 +0430

    copy workflow from oneapi

---
+++ .github/workflows/intel-scripts/build_linux.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2020 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+LANGUAGE=$1
+
+#git clone --depth 1 https://github.com/oneapi-src/oneAPI-samples.git
+
+#shellcheck disable=SC2010
+LATEST_VERSION=$(ls -1 /opt/intel/oneapi/compiler/ | grep -v latest | sort | tail -1)
+# shellcheck source=/dev/null
+source /opt/intel/oneapi/compiler/"$LATEST_VERSION"/env/vars.sh
+
+case $LANGUAGE in
+c++)
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DJSON_BuildTests=On
+  cmake --build build --parallel 10
+  cd build ; ctest -j 10 --output-on-failure
+  # cd oneAPI-samples/DirectProgramming/C++/CompilerInfrastructure/Intrinsics
+  # make && make run && make clean && make CC='icx -msse3' && make run
+  ;;
+fortran)
+  cd oneAPI-samples/DirectProgramming/Fortran/CombinationalLogic/openmp-primes
+  make && make run && make clean && make FC=ifx && make run
+  ;;
+dpc++)
+#shellcheck disable=SC2010
+  LATEST_VERSION=$(ls -1 /opt/intel/oneapi/tbb/ | grep -v latest | sort | tail -1)
+# shellcheck source=/dev/null
+  source /opt/intel/oneapi/tbb/"$LATEST_VERSION"/env/vars.sh
+  cd oneAPI-samples/DirectProgramming/DPC++/DenseLinearAlgebra/vector-add
+  make all && make run
+  ;;
+esac
---
+++ .github/workflows/intel-scripts/build_windows.bat
@@ -0,0 +1,65 @@
+REM SPDX-FileCopyrightText: 2020 Intel Corporation
+REM
+REM SPDX-License-Identifier: MIT
+
+set LANGUAGE=%1
+set VS_VER=%2
+
+IF "%VS_VER%"=="2017_build_tools" (
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+)
+
+IF "%VS_VER%"=="2019_build_tools" (
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+)
+for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
+@call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
+
+REM git clone --depth 1 https://github.com/oneapi-src/oneAPI-samples.git
+
+if "%LANGUAGE%" == "c++" goto cpp
+if "%LANGUAGE%" == "fortran" goto fortran
+if "%LANGUAGE%" == "dpc++" goto dpcpp
+goto exit
+
+:cpp
+@REM cd oneAPI-samples\DirectProgramming\C++\CompilerInfrastructure\Intrinsics
+
+cmake -S . -B build -G "Visual Studio 16 2019" -A ${{ matrix.architecture }} -DJSON_BuildTests=On
+cmake --build build --parallel 10
+cd build ; ctest -j 10 --output-on-failure
+
+@REM icl -O2 src\intrin_dot_sample.cpp
+@REM icl -O2 src\intrin_double_sample.cpp
+@REM icl -O2 src\intrin_ftz_sample.cpp
+@REM intrin_dot_sample.exe && intrin_double_sample.exe && intrin_ftz_sample.exe
+@REM set RESULT=%ERRORLEVEL%
+@REM del intrin_dot_sample.exe intrin_double_sample.exe intrin_ftz_sample.exe
+@REM icx -O2 -msse3 src\intrin_dot_sample.cpp
+@REM icx -O2 -msse3 src\intrin_double_sample.cpp
+@REM icx -O2 -msse3 src\intrin_ftz_sample.cpp
+@REM intrin_dot_sample.exe && intrin_double_sample.exe && intrin_ftz_sample.exe
+@REM set /a RESULT=%RESULT%+%ERRORLEVEL%
+goto exit
+
+:fortran
+cd oneAPI-samples\DirectProgramming\Fortran\CombinationalLogic\openmp-primes
+ifort -O2 -fpp -qopenmp src\openmp_sample.f90
+openmp_sample.exe
+set RESULT=%ERRORLEVEL%
+ifx -O2 -fpp -qopenmp src\openmp_sample.f90
+openmp_sample.exe
+set /a RESULT=%RESULT%+%ERRORLEVEL%
+goto exit
+
+:dpcpp
+for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\tbb\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
+@call "C:\Program Files (x86)\Intel\oneAPI\tbb\%LATEST_VERSION%\env\vars.bat"
+cd oneAPI-samples\DirectProgramming\DPC++\DenseLinearAlgebra\vector-add
+nmake -f Makefile.win
+nmake -f Makefile.win run
+set RESULT=%ERRORLEVEL%
+goto exit
+
+:exit
+exit /b %RESULT%
